500d71c1b8e308df875193e6613614e077dfe29b
Jaiswalroshan <90549641+Jaiswalroshan@users.noreply.github.com>
2025-07-25 14:57:04 +0530

    github oath setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { signIn } from "./actions/sign-in";
+import { signOut } from "./actions/sign-out";
 
 export default function Home() {
   return (
-   <div>Home page
-   <Button>Let build discuss app</Button>
+    <div>
+      Home page
+
+      <form action={signIn}>
+        <Button type="submit">Signin</Button>
+      </form>
 
 
-   </div>
+      <form action={signOut}>
+        <Button type="submit">Signout</Button>
+      </form>
+
+
+      {/* <Button>Let build discuss app</Button> */}
+    </div>
   );
 }
